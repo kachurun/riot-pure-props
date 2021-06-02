c18311fc729e05dff769cfe47ff5e68901b77e27
Maksim Kachurin <kachurun@gmail.com>
2021-06-02 16:32:08 +0200

    Support for spread attr

--- getProps.js
+++ getProps.js
@@ -1,5 +1,9 @@
 // Can be replaced with `import { dashToCamelCase } from "@riotjs/util/strings";`
 const dashToCamelCase = (string) => {
+    if (typeof string !== 'string') {
+        return string;
+    }
+
     return string.replace(/-(\w)/g, (_, c) => c.toUpperCase())
 }
 
@@ -20,10 +24,16 @@ const getProps = (el, context, attributes = [], initialProps = {}) => {
     const domAttributes = DOMattributesToObject(el);
 
     // Evaluate dynamic attributes
-    const dynamicAttributes = attributes.reduce((acc, { name, evaluate }) => ({
-        ...acc,
-        [dashToCamelCase(name)]: evaluate(context),
-    }), {});
+    const dynamicAttributes = attributes.reduce((acc, { name, evaluate }) => {
+        if (name == null && typeof evaluate === 'function') {
+            acc = { ...acc, ...evaluate(context) };
+        }
+        else {
+            acc[dashToCamelCase(name)] = evaluate(context);
+        }
+
+        return acc;
+    }, {});
 
     // evaluate static DOM attributes
     // merge the props eventually passed via riot.mount
